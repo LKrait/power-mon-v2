170ed3b554e271a1de026ece36fae566f493944f
LKrait <44958160+LKrait@users.noreply.github.com>
2026-09-20 04:15:44 +1000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,25 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
-android { namespace = "com.nestle.plantpowermonitoring"; compileSdk = 35
-    defaultConfig { applicationId = "com.nestle.plantpowermonitoring"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.nestle.plantpowermonitoring"
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.nestle.plantpowermonitoring"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
 
 dependencies {
